b85c214ac7fe4877bdc66ff239755982415318a1
sebas-luna <plunas@miumg.edu.gt>
2024-09-08 19:30:45 -0600

    prueba

--- CapaVista/Consulta.Designer.cs
+++ CapaVista/Consulta.Designer.cs
@@ -50,19 +50,21 @@ namespace CapaVista
             // Dgv
             // 
             this.Dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Dgv.Location = new System.Drawing.Point(311, 12);
+            this.Dgv.Location = new System.Drawing.Point(224, 10);
+            this.Dgv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Dgv.Name = "Dgv";
             this.Dgv.RowHeadersWidth = 51;
             this.Dgv.RowTemplate.Height = 24;
-            this.Dgv.Size = new System.Drawing.Size(582, 444);
+            this.Dgv.Size = new System.Drawing.Size(436, 361);
             this.Dgv.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(144, 353);
+            this.button1.Location = new System.Drawing.Point(108, 287);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 44);
+            this.button1.Size = new System.Drawing.Size(77, 36);
             this.button1.TabIndex = 1;
             this.button1.Text = "Actualizar";
             this.button1.UseVisualStyleBackColor = true;
@@ -71,116 +73,129 @@ namespace CapaVista
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 22);
+            this.label1.Location = new System.Drawing.Point(14, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Código";
             // 
             // Nombre
             // 
             this.Nombre.AutoSize = true;
-            this.Nombre.Location = new System.Drawing.Point(19, 74);
+            this.Nombre.Location = new System.Drawing.Point(14, 60);
+            this.Nombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Nombre.Name = "Nombre";
-            this.Nombre.Size = new System.Drawing.Size(58, 17);
+            this.Nombre.Size = new System.Drawing.Size(44, 13);
             this.Nombre.TabIndex = 3;
             this.Nombre.Text = "Nombre";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 124);
+            this.label3.Location = new System.Drawing.Point(14, 101);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.Size = new System.Drawing.Size(40, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Puesto";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(19, 178);
+            this.label4.Location = new System.Drawing.Point(14, 145);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 17);
+            this.label4.Size = new System.Drawing.Size(74, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "Departamento";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(19, 241);
+            this.label5.Location = new System.Drawing.Point(14, 196);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.Size = new System.Drawing.Size(40, 13);
             this.label5.TabIndex = 6;
             this.label5.Text = "Estado";
             // 
             // codigoEmp
             // 
-            this.codigoEmp.Location = new System.Drawing.Point(126, 17);
+            this.codigoEmp.Location = new System.Drawing.Point(94, 14);
+            this.codigoEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.codigoEmp.Name = "codigoEmp";
-            this.codigoEmp.Size = new System.Drawing.Size(100, 22);
+            this.codigoEmp.Size = new System.Drawing.Size(76, 20);
             this.codigoEmp.TabIndex = 7;
             // 
             // nombreEmp
             // 
-            this.nombreEmp.Location = new System.Drawing.Point(126, 69);
+            this.nombreEmp.Location = new System.Drawing.Point(94, 56);
+            this.nombreEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nombreEmp.Name = "nombreEmp";
-            this.nombreEmp.Size = new System.Drawing.Size(100, 22);
+            this.nombreEmp.Size = new System.Drawing.Size(76, 20);
             this.nombreEmp.TabIndex = 8;
             // 
             // puestoEmp
             // 
-            this.puestoEmp.Location = new System.Drawing.Point(126, 118);
+            this.puestoEmp.Location = new System.Drawing.Point(94, 96);
+            this.puestoEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.puestoEmp.Name = "puestoEmp";
-            this.puestoEmp.Size = new System.Drawing.Size(100, 22);
+            this.puestoEmp.Size = new System.Drawing.Size(76, 20);
             this.puestoEmp.TabIndex = 9;
             // 
             // deptoEmp
             // 
-            this.deptoEmp.Location = new System.Drawing.Point(126, 173);
+            this.deptoEmp.Location = new System.Drawing.Point(94, 141);
+            this.deptoEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.deptoEmp.Name = "deptoEmp";
-            this.deptoEmp.Size = new System.Drawing.Size(100, 22);
+            this.deptoEmp.Size = new System.Drawing.Size(76, 20);
             this.deptoEmp.TabIndex = 10;
             // 
             // cboEstadoEmp
             // 
             this.cboEstadoEmp.FormattingEnabled = true;
-            this.cboEstadoEmp.Location = new System.Drawing.Point(126, 232);
+            this.cboEstadoEmp.Location = new System.Drawing.Point(94, 188);
+            this.cboEstadoEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboEstadoEmp.Name = "cboEstadoEmp";
-            this.cboEstadoEmp.Size = new System.Drawing.Size(121, 24);
+            this.cboEstadoEmp.Size = new System.Drawing.Size(92, 21);
             this.cboEstadoEmp.TabIndex = 12;
             // 
             // insertarEmp
             // 
-            this.insertarEmp.Location = new System.Drawing.Point(22, 353);
+            this.insertarEmp.Location = new System.Drawing.Point(16, 287);
+            this.insertarEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.insertarEmp.Name = "insertarEmp";
-            this.insertarEmp.Size = new System.Drawing.Size(100, 44);
+            this.insertarEmp.Size = new System.Drawing.Size(75, 36);
             this.insertarEmp.TabIndex = 13;
             this.insertarEmp.Text = "Insertar";
             this.insertarEmp.UseVisualStyleBackColor = true;
             // 
             // editarEmp
             // 
-            this.editarEmp.Location = new System.Drawing.Point(22, 412);
+            this.editarEmp.Location = new System.Drawing.Point(16, 335);
+            this.editarEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.editarEmp.Name = "editarEmp";
-            this.editarEmp.Size = new System.Drawing.Size(95, 44);
+            this.editarEmp.Size = new System.Drawing.Size(71, 36);
             this.editarEmp.TabIndex = 14;
             this.editarEmp.Text = "Editar";
             this.editarEmp.UseVisualStyleBackColor = true;
             // 
             // eliminarEmp
             // 
-            this.eliminarEmp.Location = new System.Drawing.Point(144, 413);
+            this.eliminarEmp.Location = new System.Drawing.Point(108, 336);
+            this.eliminarEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.eliminarEmp.Name = "eliminarEmp";
-            this.eliminarEmp.Size = new System.Drawing.Size(112, 43);
+            this.eliminarEmp.Size = new System.Drawing.Size(84, 35);
             this.eliminarEmp.TabIndex = 15;
             this.eliminarEmp.Text = "Eliminar";
             this.eliminarEmp.UseVisualStyleBackColor = true;
             // 
             // Consulta
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(916, 475);
+            this.ClientSize = new System.Drawing.Size(687, 386);
             this.Controls.Add(this.eliminarEmp);
             this.Controls.Add(this.editarEmp);
             this.Controls.Add(this.insertarEmp);
@@ -196,6 +211,7 @@ namespace CapaVista
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Dgv);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Consulta";
             this.Text = "CREUD Empleados";
             this.Load += new System.EventHandler(this.Consulta_Load);
